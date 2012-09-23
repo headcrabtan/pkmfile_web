2fd25c1ab0af2672cf55f3bb3f75941258ca8065
Junyoung.Suh <headcrabtan@gmail.com>
2012-09-23 17:45:49 +0900

    add readme+some changes
    yes

--- pkm_core.py
+++ pkm_core.py
@@ -1,9 +1,12 @@
 # -*-coding: cp949 -*-
 from pkmtohex import pkmtohex
 import pokeclass
-#from pkm_converter import process
+from pkm_converter import process
 
-val = pkmtohex(r'C:\Users\jwsuh\Desktop\nintales.pkm')
-print  val[0x38],val[0x39],val[0x3A],val[0x3B]
+val = pkmtohex(r'C:\Users\jwsuh\Desktop\unknown.pkm')
+poke = process(val)
 
-
+print bin(int(val[64],16))[2:].zfill(8)
+print poke.female
+print poke.genderless
+print poke.fateful_encounter
---
+++ readme.txt
@@ -0,0 +1,5 @@
+used website links
+
+http://projectpokemon.org/wiki/Pokemon_Black/White_NDS_Structure : summary of the 5gen pkm file structure
+http://bulbapedia.bulbagarden.net/wiki/Individual_values#Generation_V
+...adding
