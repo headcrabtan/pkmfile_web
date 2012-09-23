226d8fd9bca09f8492dd1ed1b5b4d81dfae8878b
Junyoung.Suh <headcrabtan@gmail.com>
2012-09-23 15:43:30 +0900

    중간저장
    프큼변환함수50퍼센트완성

--- pkm_core.py
+++ pkm_core.py
@@ -1,8 +1,9 @@
 # -*-coding: cp949 -*-
 from pkmtohex import pkmtohex
 import pokeclass
-from pkm_converter import process
+#from pkm_converter import process
 
 val = pkmtohex(r'C:\Users\jwsuh\Desktop\nintales.pkm')
-print process(val).pid
+print  val[52],val[53],val[54],val[55]
+
 
