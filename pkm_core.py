a33735e5601717021447413a93befc3b78fbe762
Junyoung.Suh <headcrabtan@gmail.com>
2012-09-23 20:44:50 +0900

    keep completing stuff
    d

--- pkm_core.py
+++ pkm_core.py
@@ -3,10 +3,7 @@ from pkmtohex import pkmtohex
 import pokeclass
 from pkm_converter import process
 
-val = pkmtohex(r'C:\Users\jwsuh\Desktop\unknown.pkm')
+val = pkmtohex(r'C:\Users\jwsuh\Desktop\nintales.pkm')
 poke = process(val)
 
-print bin(int(val[64],16))[2:].zfill(8)
-print poke.female
-print poke.genderless
-print poke.fateful_encounter
+print poke
