c22ed33ae84af72fd039f063cc29643abceebf37
서준영 <headcrabtan@gmail.com>
2012-12-28 16:45:40 +0900

    컨버터 완성

--- pkm_core.py
+++ pkm_core.py
@@ -3,7 +3,8 @@ from pkmtohex import pkmtohex
 import pokeclass
 from pkm_converter import process
 
-val = pkmtohex(r'C:\Users\jwsuh\Desktop\ninetales2.pkm')
-poke = process(val)
-
-print poke.nickname
+def pkmparse(path)
+	val = pkmtohex(path)
+	poke = process(val)
+	return poke
+pkmparse('C:\Keldeo.pkm')
